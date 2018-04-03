649d7610e9b78a6fee13c3881115859fe079527d
Ken Liao <kenliao94@gmail.com>
2018-04-03 21:10:03 +0000

    change path

--- leakage_detector.py
+++ leakage_detector.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 import dataset_processing.parameters as parameters
 
-HOG = door_detector.HOGDetector("door_detection/hog_door_detector/model")
+HOG = door_detector.HOGDetector(os.path.dirname(os.path.realpath(_file_)) + "/door_detection/hog_door_detector/model")
 pa = parameters.Parameters()
 
 def process(image_path):
@@ -19,4 +19,6 @@ def process(image_path):
         label.label(pa,image_path,preds[0],preds[2],preds[1],preds[3],im_name=output_image_path)
     return output_image_path
     
-    
+
+if __name__ == "__main__":
+    process("dataset/images/raw_images/img_thermal_1519937382264.jpg")
